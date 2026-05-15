d322decc5e2a43ce0d7b1f04fb0e37d4b64d1294
Danek <janek@projmen.pl>
2026-05-15 17:51:03 +0200

    nasze miasto cities

--- scrapers/nasze_miasto_scaper.py
+++ scrapers/nasze_miasto_scaper.py
@@ -1,6 +1,9 @@
+import time
+
 from bs4 import BeautifulSoup
 from curl_cffi import requests as r
 
+from scrapers.url.file_url_client import FileUrlClient
 from scrapers.url.url_client import UrlClient
 
 
@@ -13,10 +16,7 @@ class NaszeMiastoScraper:
 
     def __init__(self, url_client: UrlClient):
         self.url_client: UrlClient = url_client
-
-    def get_from_city(self, city: str, page: int) -> list[str]:
-        url = f"https://{city}.naszemiasto.pl/wiadomosci/{page}"
-        headers = {
+        self.headers = {
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
             'accept-language': 'en-GB,en;q=0.8',
             'priority': 'u=0, i',
@@ -31,15 +31,52 @@ class NaszeMiastoScraper:
             'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36',
         }
+        self.max_pages = 5
 
-        response = r.get(url, headers=headers, impersonate='chrome')
-        pass
+    def _get_all_cities(self):
+        url = 'https://poznan.naszemiasto.pl/ajax/nsk/geo/cities'
+        response = r.get(url, headers=self.headers, impersonate='chrome')
+        soup = BeautifulSoup(response.text, 'html.parser')
+        all_links = soup.find_all('a')
+        groups = [link['data-name'].replace('-', '') for link in soup.find_all('li')]
+        cities = [link['data-name'] for link in all_links]
+        cities = [c for c in cities if c not in groups]
+        return cities
+
+    def _get_from_city_from_page(self, city: str, page: int) -> list[str]:
+        url = f"https://{city}.naszemiasto.pl/wiadomosci/{page}"
+
+        response = r.get(url, headers=self.headers, impersonate='chrome')
         soup = BeautifulSoup(response.text, 'html.parser')
         articles_section = soup.find('section', attrs={'data-ea': 'articles_left'})
 
         all_urls = articles_section.find_all('a', attrs={'data-ec': "atomsListingArticleTileWithSeparatedLink"})
         all_urls = [article['href'] for article in all_urls if 'href' in article.attrs and article['href'].startswith('/')]
+        all_urls = [f"https://{city}.naszemiasto.pl{url}" for url in all_urls]
 
         return list(set(all_urls))  # remove duplicates
 
+    def collect_urls_from_city(self, city: str):
+        page = 1  # Starts from page 1
+        urls = []
+        for _ in range(self.max_pages):
+            response_urls = self._get_from_city_from_page(city, page)
+            urls.extend(response_urls)
+            some_url_exists = self.url_client.any_url_exists(response_urls)
+            for url in response_urls:
+                self.url_client.add_url(url)
+            if not response_urls or some_url_exists:
+                break
+            page += 1
+            time.sleep(1)  # just in case
 
+
+def main():
+    scraper = NaszeMiastoScraper(FileUrlClient('nasze_miasto.txt'))
+    cities = scraper._get_all_cities()
+    for city in cities:
+        scraper.collect_urls_from_city(city)
+
+
+if __name__ == '__main__':
+    main()
